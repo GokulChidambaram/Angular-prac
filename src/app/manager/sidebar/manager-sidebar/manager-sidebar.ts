edf6a33247fac00194ec771e701dc9dc8003816e
GokulChidambaram <gokulchidambaram14@gmail.com>
2026-03-05 08:41:29 +0530

    manager lay

---
+++ src/app/manager/sidebar/manager-sidebar/manager-sidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-sidebar',
+  imports: [],
+  templateUrl: './manager-sidebar.html',
+  styleUrl: './manager-sidebar.scss',
+})
+export class ManagerSidebar {
+
+}
